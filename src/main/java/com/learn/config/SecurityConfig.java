d4db3f13cba09a693a95fab53b5c84413121f369
sofianebraneci <braneci1998@gmail.com>
2024-06-16 19:31:48 +0200

    Security Config

---
+++ src/main/java/com/learn/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.learn.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.httpBasic(Customizer.withDefaults());
+        http.authorizeHttpRequests(c -> c
+                .requestMatchers(new AntPathRequestMatcher("/users/**"))
+                    .authenticated()
+                .requestMatchers(new AntPathRequestMatcher("/greetings/**"))
+                    .permitAll());
+
+        return http.build();
+    }
+}
