0425d0f163495b1dd690aa8a20b71ff60d42b16b
sofianebraneci <braneci1998@gmail.com>
2024-06-23 12:41:17 +0200

    security filter using Jakarta Filter

--- src/main/java/com/learn/security/AuthenticationLoggingFilter.java
+++ src/main/java/com/learn/security/AuthenticationLoggingFilter.java
@@ -2,26 +2,25 @@ package com.learn.security;
 
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletRequestWrapper;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.Objects;
 
-public class ResquestValidationFilter implements Filter {
+@Slf4j
+public class AuthenticationLoggingFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-
+        // here we are sure that we have a request id
         String requestId = request.getHeader("Request-Id");
-        if(Objects.isNull(requestId) || requestId.isBlank()){
-            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            return;
-        }
-        CustomHttpServletWrapper wrapper = new CustomHttpServletWrapper(request);
-        wrapper.addHeader("START", String.valueOf(System.currentTimeMillis()));
-        filterChain.doFilter(wrapper, servletResponse);
+        long start = Long.parseLong(request.getHeader("START"));
+        log.info("Successfully authenticated request with id {}, time = {}ms", requestId,
+                (System.currentTimeMillis() - start));
+        filterChain.doFilter(servletRequest, servletResponse);
+
+
     }
 }
